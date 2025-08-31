899e12bc24327943c21fdfbdca8954a625380e52
vamsikilari777 <kilarivamsikvs777@gmail.com>
2025-08-31 19:46:24 +0530

    first commit

---
+++ src/service/authService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+const API_BASE = import.meta.env.VITE_API_BASE;
+
+const API_URL = `${API_BASE}/api`;
+
+export const registerUser = async (data) => {
+    try {
+        const response = await axios.post(
+            API_URL+"/register",
+            data
+          );
+        return response
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const login = async (data) => {
+    try {
+        const response = await axios.post(API_URL+"/login", data);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
---
+++ src/util/contants.js
@@ -0,0 +1,1 @@
+export const RAZORPAY_KEY = "rzp_test_R5uvMzRhwfZQfe";
